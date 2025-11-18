300cf3537444dd3d5a7d0b2134cfb3998243a7df
mbewepromise <157880801+mbewepromise@users.noreply.github.com>
2025-11-18 14:44:03 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+$module['name']        = 'Timesheet Selfie';
+$module['module_name'] = 'timesheet_selfie';
+$module['author']      = 'Perfex User';
+$module['version']     = '1.0.0';
+$module['description'] = 'Staff clock-in/out + break/lunch with selfie, IP whitelist, voice prompts.';
+$module['requires_core'] = '3.0.0';
